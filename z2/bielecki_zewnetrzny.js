ae2f0c1078c962a5f9b4bca1f2227294119702f7
ROCKIE2115 <rockiezse@gmail.com>
2025-09-15 20:42:58 +0200

    Add files via upload

--- z2/bielecki_zewnetrzny.js
+++ z2/bielecki_zewnetrzny.js
@@ -1,2 +1,6 @@
 document.write("ostatnia modyfikacja strony".fontcolor("blue").bold().strike().fontsize(5)+"<br>"); 
 document.write(document.lastModified.bold().strike().fontcolor("blue").fontsize(5));
+function okno_zamknij_bielecki()  
+{ 
+window.close() 
+} 
